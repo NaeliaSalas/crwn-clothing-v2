6e817a0d69efc6997999c8d3795756eaa4864b2f
Naelia Salas <naeliasalas@gmail.com>
2023-01-13 10:08:01 +0100

    Add firebase db

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,40 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBEt_B4LWE9ivOVD8SE6D-t0p4VIVuL7K4",
+  authDomain: "crwn-clothing-db-af528.firebaseapp.com",
+  projectId: "crwn-clothing-db-af528",
+  storageBucket: "crwn-clothing-db-af528.appspot.com",
+  messagingSenderId: "921196226072",
+  appId: "1:921196226072:web:efe0073a3ef794fd5b456d",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapShot = await getDoc(userDocRef);
+  console.log(userSnapShot);
+  console.log(userSnapShot.exists());
+};
