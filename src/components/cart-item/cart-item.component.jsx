cb9cedfcf993beeb4de63cb46159a05ea3593217
Naelia Salas <naeliasalas@gmail.com>
2023-02-18 15:24:12 +0100

    added in out addtocart feature

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,20 @@
+import "./cart-item.styles.scss";
+
+const CartItem = ({ cartItem }) => {
+  const { name, imgUrl, price, quantity } = cartItem;
+  return (
+    <div className='cart-item-container'>
+      <img src={imgUrl} alt={`${name}`} />
+      <div className='item-details'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+
+        <span>
+          {quantity} x ${price}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
